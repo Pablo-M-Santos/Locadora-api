a1e5bbed43794eded81563599ac4ae5577b26eea
Pablo <pablomoreirasantos.hp@gmail.com>
2024-11-28 14:04:49 -0300

    logo

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -26,7 +26,7 @@
           </div>
 
           <div class="container-interno">
-            <img src="/locadoraAPIWDA/src/assets/logo.png"  alt="Logo"/>
+            <img src="/locadoraAPIWDA/public/logo.png"  alt="Logo"/>
             <h2>Seja bem-vindo!</h2>
           </div>
         </div>
